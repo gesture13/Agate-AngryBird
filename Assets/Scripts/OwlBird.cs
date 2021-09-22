f5aa36b350205421bda795cbf01773f18eb59804
gesture13 <aputraherdani@gmail.com>
2021-09-22 12:11:51 +0700

    first commit

---
+++ Assets/Scripts/OwlBird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+
+    public float delay = 3f;
+    float countdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Debug.Log("BOOM");
+    }
+}
